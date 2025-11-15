590332c90858a2accf2142a02b1c8b5334c2b5b3
davidzakii <davidzaki77@gmail.com>
2025-11-15 11:30:07 +0200

    feat:Add front end link in backend cors orign

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://quiz-task-blush.vercel.app',
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
